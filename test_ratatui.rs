bf8816840789f4b72dd1319e60b64153a59587c6
BeetleBot <nikarabilla@gmail.com>
2026-05-19 11:18:01 +0530

    .

--- test_ratatui.rs
+++
@@ -1,7 +0,0 @@
-use ratatui::widgets::{BarChart, BarGroup, Bar};
-fn test() {
-    let g1 = BarGroup::default().bars(&[Bar::default().value(1)]);
-    let groups = vec![g1];
-    let b = BarChart::default();
-    // How to assign groups to b?
-}
